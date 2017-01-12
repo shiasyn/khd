d941ae85aa429f807dc96617f50e42c9cfe2bb44
koekeishiya <aasvi93@hotmail.com>
2017-01-12 11:58:59 +0100

    version 1.1.3

--- src/khd.cpp
+++ src/khd.cpp
@@ -17,7 +17,7 @@ extern "C" bool CGSIsSecureEventInputSet();
 #define IsSecureKeyboardEntryEnabled CGSIsSecureEventInputSet
 
 internal CFMachPortRef KhdEventTap;
-internal const char *KhdVersion = "1.1.2";
+internal const char *KhdVersion = "1.1.3";
 
 modifier_state ModifierState = {};
 mode DefaultBindingMode = {};
